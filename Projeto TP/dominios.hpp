a4aae39420ccbbf36b17aaaabb1cbd75b220ccba
LuizCarlos790 <lcca790@gmail.com>
2025-05-01 13:54:05 -0300

    Update dominios.hpp
    Adicionado 5 classes dominio.

--- Projeto TP/dominios.hpp
+++ Projeto TP/dominios.hpp
@@ -9,6 +9,7 @@
 #define dominios_hpp
 
 #include <stdio.h>
+#include <string>
 using namespace std;
 //Classe Codigo
 class Codigo{
@@ -55,4 +56,95 @@ inline string Perfil::getValor(){
 class Senha{
     
 };
+
+//Domínio: Quantidade / Luiz Carlos - 241004560
+class Quantidade {
+private:
+    static constexpr float LIMITE_MAX = 1000000;
+    static constexpr float LIMITE_MIN = 0.01;
+    float valor;
+    bool validar(float);
+public:
+    bool setValor(float);
+    float getValor();
+
+};
+
+inline float Quantidade::getValor(){
+    return valor;
+
+};
+
+// Domínio: Dinheiro / Luiz Carlos - 241004560
+class Dinheiro {
+private:
+    static const int LIMITE_MAX = 1000000;
+    static const int LIMITE_MIN = 1;
+    int valor;
+    bool validar(int);
+public:
+    bool setValor(int);
+    int getValor();
+
+};
+
+inline int Dinheiro::getValor(){
+    return valor;
+
+};
+
+// Domínio: Data / Luiz Carlos - 241004560
+class Data {
+private:
+
+    static const int LIMITE_MAX_ANO = 2025;
+    static const int LIMITE_MIN_ANO = 1;
+    static const int LIMITE_MAX_MES = 12;
+    static const int LIMITE_MIN_MES = 1;
+    static const int LIMITE_MAX_DIA = 31;
+    static const int LIMITE_MIN_DIA = 1;
+    int ano, mes, dia;
+    string data;
+    bool validarData(int, int, int);
+
+public:
+    bool setData(int, int, int);
+    string getData();
+};
+
+inline string Data::getData(){
+    return data;}
+
+// Dominio: Nome / Luiz Carlos - 241004560
+class Nome {
+private:
+    string nome;
+    static const int LIMITE_CHAR_MAX = 20;
+    static const int LIMITE_CHAR_MIN = 1;
+    bool validarNome(string);
+public:
+    bool setNome(string);
+    string getNome();
+};
+
+inline string Nome::getNome(){
+    return nome;
+}
+
+//Dominio: CPF / Luiz Carlos - 241004560
+
+class CPF {
+private:
+    static const int TAMANHO = 11;
+    string cpf;
+    bool validarCPF(string);
+public:
+    bool setCPF(string);
+    string getCPF();
+
+};
+
+inline string CPF::getCPF(){
+    return cpf;
+}
 #endif /* dominios_hpp */
