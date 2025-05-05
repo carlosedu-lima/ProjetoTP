688c5304c63ef833ed2dbe53530182e90a449cbf
LuizCarlos790 <lcca790@gmail.com>
2025-05-04 22:56:06 -0300

    Create entidades.hpp
    Adicionado o file entidades.hpp.
    Implementado parcialmente a entidade Ordem.

---
+++ Projeto TP/entidades.hpp
@@ -0,0 +1,21 @@
+#ifndef ENTIDADES_HPP_INCLUDED
+#define ENTIDADES_HPP_INCLUDED
+
+#include "dominios.hpp"
+using namespace std;
+
+// Entidade: Ordem / Luiz Carlos - 241004560
+class Ordem{
+private:
+    Codigo codigo;
+    CodigoNegociacao codigonegociacao;
+    Data data;
+    Dinheiro preço;
+    Quantidade quantidade;
+public:
+    // Metodos
+};
+
+
+
+#endif // ENTIDADES_HPP_INCLUDED
