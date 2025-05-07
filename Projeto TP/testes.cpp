2d94a0fd74e3e6dd1864ca455701f36794e31fd8
LuizCarlos790 <lcca790@gmail.com>
2025-05-06 22:00:15 -0300

    Create testes.cpp
    Adicionado o arquivo testes.cpp
    adicionado métodos do teste do dominio dinheiro.

---
+++ Projeto TP/testes.cpp
@@ -0,0 +1,39 @@
+#include "testes.hpp"
+
+void TUDinheiro::setUp(){
+    dinheiro = new Dinheiro();
+    estado = SUCESSO;
+}
+void TUDinheiro::tearDown(){
+    delete dinheiro;
+}
+
+void TUDinheiro::testarCenarioValorValido(){
+    try{
+        dinheiro->setValor(VALOR_VALIDO);
+        if(dinheiro->getValor()!= VALOR_VALIDO)
+            estado = FALHA;
+    }
+    catch(invalid_argument e){
+        estado = FALHA;
+    }
+}
+
+void TUDinheiro::testarCenarioValorInvalido(){
+    try{
+        dinheiro->setValor(VALOR_INVALIDO);
+        if(dinheiro->getValor() == VALOR_INVALIDO)
+            estado = FALHA;
+    }
+    catch(invalid_argument e){
+        estado = FALHA;
+    }
+}
+
+int TUDinheiro::run(){
+    setUp();
+    testarCenarioValorValido();
+    testarCenarioValorInvalido();
+    tearDown();
+    return estado;
+}
