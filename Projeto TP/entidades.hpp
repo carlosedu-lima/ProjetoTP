9997ed2bb22d09dd15b048f8c3eceb971c6101bd
Carlos Eduardo Figueredo Lima <82738937+carlosedu-lima@users.noreply.github.com>
2025-05-07 22:08:52 -0300

    Update entidades.hpp
    Criacao classes e metos conta e carteira

--- Projeto TP/entidades.hpp
+++ Projeto TP/entidades.hpp
@@ -2,6 +2,7 @@
 #define ENTIDADES_HPP_INCLUDED
 
 #include "dominios.hpp"
+
 using namespace std;
 
 // Entidade: Ordem / Luiz Carlos - 241004560
@@ -15,7 +16,68 @@ private:
 public:
     // Metodos
 };
-
-
-
+//Classe e Metodos Entidade Conta - Carlos Eduardo 241004659
+class Conta{
+private:
+    CPF cpf;
+    Nome nome;
+    Senha senha;
+public:
+    void setCPF(CPF);
+    CPF getCPF();
+    void setNome(Nome);
+    Nome getNome();
+    void setSenha(Senha);
+    Senha getSenha();
+};
+inline void Conta::setCPF(CPF cpf){
+    this->cpf=cpf;
+}
+inline CPF Conta::getCPF(){
+    return cpf;
+}
+inline void Conta::setNome(Nome nome){
+    this->nome=nome;
+}
+inline Nome Conta::getNome(){
+    return nome;
+}
+inline void Conta::setSenha(Senha senha){
+    this->senha=senha;
+}
+inline Senha Conta::getSenha(){
+    return senha;
+}
+//Classe e Metodos Entidade Carteira - Carlos Eduardo 241004659
+class Carteira{
+private:
+    Codigo codigo;
+    Nome nome;
+    Perfil perfil;
+public:
+    void setCodigo(Codigo);
+    Codigo getCodigo();
+    void setNome(Nome);
+    Nome getNome();
+    void setPerfil(Perfil);
+    Perfil getPerfil();
+};
+inline void Carteira::setCodigo(Codigo codigo){
+    this->codigo=codigo;
+}
+inline Codigo Carteira::getCodigo(){
+    return codigo;
+}
+inline void Carteira::setNome(Nome nome){
+    this->nome=nome;
+}
+inline Nome Carteira::getNome(){
+    return nome;
+}
+inline void Carteira::setPerfil(Perfil perfil){
+    this->perfil=perfil;
+}
+inline Perfil Carteira::getPerfil(){
+    return perfil;
+}
 #endif // ENTIDADES_HPP_INCLUDED
