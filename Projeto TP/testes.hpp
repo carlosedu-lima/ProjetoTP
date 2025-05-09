15963adcc1d19a0200ef2fb45f57640eebf2263a
LuizCarlos790 <lcca790@gmail.com>
2025-05-09 15:01:54 -0300

    Update testes.hpp
    Adicionado Teste Data.

--- Projeto TP/testes.hpp
+++ Projeto TP/testes.hpp
@@ -4,7 +4,7 @@
 #include "dominios.hpp"
 using namespace std;
 
-// TESTE Dominio: Dinheiro // Luiz Carlos - 241004560
+/// TESTE Dominio: Dinheiro // Luiz Carlos - 241004560
 class TUDinheiro {
 private:
     const static int VALOR_VALIDO = 1000;
@@ -21,7 +21,7 @@ public:
     int run();
 
 };
-// TESTE Dominio: Quantidade // Luiz Carlos - 241004560
+/// TESTE Dominio: Quantidade // Luiz Carlos - 241004560
 class TUQuantidade {
 private:
     constexpr static float VALOR_VALIDO = 1000;
@@ -39,5 +39,25 @@ public:
 
 };
 
+/// TESTE Dominio: Data // Luiz Carlos - 241004560
+class TUData{
+    private:
+    const static int VALOR_VALIDO_ANO = 1998;
+    const static int VALOR_INVALIDO_ANO = -1 ;
+    const static int VALOR_VALIDO_MES = 3 ;
+    const static int VALOR_INVALIDO_MES = 13;
+    const static int VALOR_VALIDO_DIA = 9 ;
+    const static int VALOR_INVALIDO_DIA = 32;
+    Data *data;
+    int estado;
+    void setUp();
+    void tearDown();
+    void testarCenarioValorValido();
+    void testarCenarioValorInvalido();
+public:
+    const static int SUCESSO = 0;
+    const static int FALHA = -1;
+    int run();
+};
 
 #endif // TESTES_HPP_INCLUDED
