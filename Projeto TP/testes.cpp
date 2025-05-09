2ccdae2db0bc136c4df739841ac2a2a193b6d1b3
LuizCarlos790 <lcca790@gmail.com>
2025-05-09 14:20:15 -0300

    Update testes.cpp
    Bugfix testes;
    Adicionado mais exceções para depuração;
    Adicionado mais um dominio em teste;

--- Projeto TP/testes.cpp
+++ Projeto TP/testes.cpp
@@ -1,5 +1,6 @@
 #include "testes.hpp"
-// Métodos teste Dinheiro / Luiz Carlos - 241004560
+
+/// Métodos teste Dinheiro / Luiz Carlos - 241004560
 void TUDinheiro::setUp(){
     dinheiro = new Dinheiro();
     estado = SUCESSO;
@@ -17,20 +18,75 @@ void TUDinheiro::testarCenarioValorValido(){
     catch(invalid_argument e){
         estado = FALHA;
     }
+    catch(...){
+        cout << "Erro: Exceção inesperada!" << endl;
+        estado = FALHA;
+    }
 }
 
 void TUDinheiro::testarCenarioValorInvalido(){
     try{
         dinheiro->setValor(VALOR_INVALIDO);
+        estado = FALHA;
+    }
+    catch(invalid_argument){
         if(dinheiro->getValor() == VALOR_INVALIDO)
             estado = FALHA;
     }
-    catch(invalid_argument e){
+    catch(...){
+        cout << "Erro: Exceção inesperada!" << endl;
         estado = FALHA;
     }
 }
 
 int TUDinheiro::run(){
+    setUp();
+    testarCenarioValorValido();
+    testarCenarioValorInvalido();
+    tearDown();
+    return estado;
+}
+
+/// Métodos teste Quantidade / Luiz Carlos - 241004560
+void TUQuantidade::setUp(){
+    quantidade = new Quantidade();
+    estado = SUCESSO;
+}
+void TUQuantidade::tearDown(){
+    delete quantidade;
+}
+
+void TUQuantidade::testarCenarioValorValido(){
+    try{
+        quantidade->setValor(VALOR_VALIDO);
+        if(quantidade->getValor()!= VALOR_VALIDO)
+            estado = FALHA;
+    }
+    catch(invalid_argument e){
+        estado = FALHA;
+    }
+    catch(...){
+        cout << "Erro: Exceção inesperada!" << endl;
+        estado = FALHA;
+    }
+}
+
+void TUQuantidade::testarCenarioValorInvalido(){
+    try{
+        quantidade->setValor(VALOR_INVALIDO);
+        estado = FALHA;
+    }
+    catch(invalid_argument){
+        if(quantidade->getValor() == VALOR_INVALIDO)
+            estado = FALHA;
+    }
+    catch(...){
+        cout << "Erro: Exceção inesperada!" << endl;
+        estado = FALHA;
+    }
+}
+
+int TUQuantidade::run(){
     setUp();
     testarCenarioValorValido();
     testarCenarioValorInvalido();
