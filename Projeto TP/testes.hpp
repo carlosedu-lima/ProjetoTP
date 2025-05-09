a6bde64dd4ca459e836434516c8a0595ffd02e76
LuizCarlos790 <lcca790@gmail.com>
2025-05-09 14:21:02 -0300

    Update testes.hpp
    Adicionado Teste Unitário de Quantidade.

--- Projeto TP/testes.hpp
+++ Projeto TP/testes.hpp
@@ -3,13 +3,30 @@
 #include <stdexcept>
 #include "dominios.hpp"
 using namespace std;
+
 // TESTE Dominio: Dinheiro // Luiz Carlos - 241004560
-// ainda não funciona
 class TUDinheiro {
 private:
-    const static int VALOR_VALIDO = 100;
+    const static int VALOR_VALIDO = 1000;
     const static int VALOR_INVALIDO = -1;
     Dinheiro *dinheiro;
+    int estado;
+    void setUp();
+    void tearDown();
+    void testarCenarioValorValido();
+    void testarCenarioValorInvalido();
+public:
+    const static int SUCESSO = 0;
+    const static int FALHA = -1;
+    int run();
+
+};
+// TESTE Dominio: Quantidade // Luiz Carlos - 241004560
+class TUQuantidade {
+private:
+    constexpr static float VALOR_VALIDO = 1000;
+    constexpr static float VALOR_INVALIDO = -1;
+    Quantidade *quantidade;
     int estado;
     void setUp();
     void tearDown();
