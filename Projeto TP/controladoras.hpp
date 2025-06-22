1a39d696e52a826a2fea881212839a7da25c8ece
LuizCarlos790 <lcca790@gmail.com>
2025-06-22 13:39:00 -0300

    Update controladoras.hpp
    Controladora da IAA.

--- Projeto TP/controladoras.hpp
+++ Projeto TP/controladoras.hpp
@@ -1,1 +1,19 @@
+#ifndef CONTROLADORAS_HPP_INCLUDED
+#define CONTROLADORAS_HPP_INCLUDED
+#include "interfaces.hpp"
 
+class CntrIAA:public IAA{
+private:
+    ISA *cntrISA; // Referencia para camada servidor
+public:
+    bool autenticar(CPF&)override;
+    void setCntrISA(ISA*);
+
+};
+
+void inline CntrIAA::setCntrISA(ISA *cntrISA){
+    this->cntrISA = cntrISA;
+}
+
+
+#endif // CONTROLADORAS_HPP_INCLUDED
