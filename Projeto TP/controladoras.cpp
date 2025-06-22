4fb978b1128a9b1e68a548ec8130d149e5f0ec2f
LuizCarlos790 <lcca790@gmail.com>
2025-06-22 13:39:28 -0300

    Update controladoras.cpp
    Métodos CntrIAA.

--- Projeto TP/controladoras.cpp
+++ Projeto TP/controladoras.cpp
@@ -1,1 +1,27 @@
+#include "controladoras.hpp"
+
+bool CntrIAA::autenticar(CPF& cpf){
+    Senha senha;
+    string entrada;
+
+    while(true){
+        cout << endl << "Autenticacao de usuario" << endl << endl;
+
+        try{
+
+           cout << "Digite o CPF : "<< endl;
+           cin >> entrada;
+           cpf.setCPF(entrada);
+           cout << "Digite a senha : " << endl;
+           cin >> entrada;
+           senha.setValor(entrada);
+           break;}
+           catch(const invalid_argument &exp){
+               cout << endl<< "Dado em formato incorreto." << endl;
+           }
+    }
+
+    bool resultado = cntrISA->autenticar(cpf, senha);
+    return resultado;
+}
 
