bc513e9476c329158aa0b7715a7eef18ceefe0e8
LuizCarlos790 <lcca790@gmail.com>
2025-06-23 01:51:07 -0300

    Update controladoras.hpp
    CntrIAC

--- Projeto TP/controladoras.hpp
+++ Projeto TP/controladoras.hpp
@@ -15,5 +15,15 @@ void inline CntrIAA::setCntrISA(ISA *cntrISA){
     this->cntrISA = cntrISA;
 }
 
-
+class CntrIAC:public IAC{
+private:
+    ISC *cntrISC; // Referencia para camada servidor
+public:
+    bool criar()override;
+    void executar(CPF)override;
+    void setCntrISC(ISC*);
+};
+void inline CntrIAC::setCntrISC(ISC *cntrISC){
+    this->cntrISC = cntrISC;
+}
 #endif // CONTROLADORAS_HPP_INCLUDED
