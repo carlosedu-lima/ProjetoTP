708fdb53d481f0e87354bd566326c9b70b4caaab
LuizCarlos790 <lcca790@gmail.com>
2025-06-24 16:41:22 -0300

    Update stubs.hpp
    StubISC

--- Projeto TP/stubs.hpp
+++ Projeto TP/stubs.hpp
@@ -10,4 +10,18 @@ public:
     bool autenticar(CPF&,Senha&)override;
 };
 
+class StubISC:public ISC{
+private:
+    const static string TRIGGER_FALHA_CPF;
+    const static string TRIGGER_ERRO_SISTEMA_CPF;
+    const static string TRIGGER_FALHA_NOME;
+    const static string TRIGGER_ERRO_SISTEMA_NOME;
+    const static string TRIGGER_FALHA_SENHA;
+    const static string TRIGGER_ERRO_SISTEMA_SENHA;
+public:
+    bool criar(Conta)override;
+    bool ler(Conta&)override;
+    bool editar(Conta)override;
+    bool excluir(CPF)override;
+};
 #endif // STUBS_HPP_INCLUDED
